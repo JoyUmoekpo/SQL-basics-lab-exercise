c1572b7a21fbb53e2f12b389c5b720f9835a147e
JoyUmoekpo <joyumoekpo1@gmail.com>
2022-12-06 00:03:14 -0600

    Completed artist.sql

---
+++ artist.sql
@@ -0,0 +1,28 @@
+-- 1: Add 3 new artists to the artist table. (It already exists.)
+
+INSERT INTO artist (artist_id, name)
+VALUES (276, 'Kehlani'),
+(277, 'P.I.N.K.'),
+(278, 'Demi Lovato');
+
+-- 2: Select 10 artists in reverse alphabetical order.
+
+SELECT * FROM artist
+ORDER BY name DESC
+LIMIT 10;
+
+-- 3: Select 5 artists in alphabetical order.
+
+SELECT * FROM artist
+ORDER BY name ASC
+LIMIT 5;
+
+-- 4: Select all artists that start with the word ‘Black’.
+
+SELECT * FROM artist
+WHERE name LIKE 'Black%';
+
+-- 5: Select all artists that contain the word ‘Black’.
+
+SELECT * FROM artist
+WHERE name LIKE '%Black%';
